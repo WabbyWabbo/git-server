42163fe257deabfd7ef4a8e09fc6cf6cccccab80
yatung <yatung.yu@gmail.com>
2021-11-17 23:41:22 +0800

    初始化完成后的加载页面 汉化

--- server-core/src/main/java/io/onedev/server/util/init/InitStage.java
+++ server-core/src/main/java/io/onedev/server/util/init/InitStage.java
@@ -39,7 +39,7 @@ public class InitStage implements Serializable, Cloneable {
 	}
 
 	public synchronized void finished() {
-		message = "Please wait...";
+		message = "请稍候...";
 		manualConfigs.clear();
 		notify();
 	}
